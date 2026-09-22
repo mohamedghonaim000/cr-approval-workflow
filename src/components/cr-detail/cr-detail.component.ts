e64e37985ab34accaf081a2ce58b264d7ce1a509
mohamedghonaim000 <mohamedmokhtar64232@gmail.com>
2026-09-23 01:30:02 +0300

    fix: add required validator to rejectControl in CrDetailComponent

--- src/components/cr-detail/cr-detail.component.ts
+++ src/components/cr-detail/cr-detail.component.ts
@@ -1,6 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CrApiService } from '../../api/cr-api.service';
 import { SessionService } from '../../session/session.service';
 import { CrDetail, TimelineEntry } from '../../models/cr.models';
@@ -27,7 +27,7 @@ export class CrDetailComponent implements OnInit {
 	submitting = false;
 	actionError?: string;
 	// TODO: add validation so the form is invalid until a reason is entered.
-	rejectControl = new FormControl('', { nonNullable: true });
+	rejectControl = new FormControl('', { nonNullable: true ,validators:[Validators.required]});
 
 	constructor(private readonly api: CrApiService, private readonly session: SessionService) {}
 
